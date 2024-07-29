297f219cf3f844393ad65d52357541edd8b06db4
MeowMerry <meow3525@gmail.com>
2024-07-29 07:11:55 -0500

    addUpTo and arrayOfIndices

---
+++ addUpTo.js
@@ -0,0 +1,11 @@
+/*
+Q. Given an array, stack each element by adding the sum of the previous elements.
+
+Example:
+Input: [1, 2, 3, 0]"
+Output: [1, 3, 6, 6] // ["1", "1+2", "1+2+3", "1+2+3+0"]
+*/
+const addUpTo = arr => {
+
+};
+console.log(addUpTo([1, 2, 3, 0])); // [1, 3, 6, 6]
