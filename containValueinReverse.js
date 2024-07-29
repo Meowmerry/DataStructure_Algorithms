47e285e8f74cc705e90a156d7ca4058679b8fdf7
MeowMerry <meow3525@gmail.com>
2024-07-29 05:58:50 -0500

    29July 2024

---
+++ containValueinReverse.js
@@ -0,0 +1,22 @@
+/*
+Given two arrays, does the second array contain the same values as the first but in reverse? 
+If so, return true. Return false otherwise.
+
+[execution time limit] 4 seconds (js)
+
+[input] array.integer a
+
+[input] array.integer b
+
+[output] boolean
+*/
+
+
+function containValueinReverse(a, b) {
+
+}
+
+console.log(containValueinReverse([1, 2, 3], [3, 2, 1])); // Output: true
+console.log(containValueinReverse([1, 2, 3], [1, 2])); // Output: false
+console.log(containValueinReverse([1, 2, 3], [3, 2, 1, 4])); // Output: false
+console.log(containValueinReverse([1, 2, 4], [4, 2, 1])); // Output: true
---
+++ isWithinRang.js
@@ -0,0 +1,22 @@
+/* 
+Given an array of numbers and a positive number representing a range, 
+determine if all of the numbers in the array are within that range of each other. 
+Return true if so and false otherwise.
+
+For example, if the numbers are [6, 2, 8, 10, 15, 1] and the range is 16, 
+then this function returns true because all of the numbers are at least that close in value. 
+But for the same numbers, if the range is 6, 
+then the function should return false because there are multiple pairs with a larger range;
+2 & 10, 1 & 8, 6 & 15, etc.
+
+[execution time limit] 4 seconds (js)
+
+[input] array.integer array
+
+[input] integer range
+
+A positive integer
+
+[output] boolean
+
+*/
