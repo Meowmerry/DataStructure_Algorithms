bbf49a84ab2162bf1461fbbd5a230c6510ccc336
MeowMerry <meow3525@gmail.com>
2024-08-04 09:48:31 -0500

    addUpTo with Reduce

--- Array&Hashing/addUpTo.js
+++ Array&Hashing/addUpTo.js
@@ -20,7 +20,7 @@ Iterate through the input array,
 //   const outputArr = [arr[0]];
 //   for(let i = 1 ;i< arr.length; i++){
 //     outputArr.push(outputArr[i-1] + arr[i])
-    
+
 //   }
 //   return outputArr
 // };
@@ -28,7 +28,7 @@ Iterate through the input array,
 // const addUpTo = arr => {
 //     let sum = 0;
 //     return arr.map((ele)=> sum += ele)
-   
+
 // }
 
 // console.log(addUpTo([])); // []
@@ -37,19 +37,19 @@ Iterate through the input array,
 
 
 
-const addUpToRecursion = (arr, sumArr=[], i = 1) => {
-  //Base case
-  if (arr.length === 0) return [];
-  if (arr[i] === undefined) return sumArr;
-  // Recursive case
-  if (i === 1) sumArr = [arr[0]];  
-  
-  sumArr.push(sumArr[i - 1] + arr[i]);
-  return addUpToRecursion(arr, sumArr, i + 1);
-}
+const addUpToRecursion = (arr, sumArr = [], i = 1) => {
+    //Base case
+    if (arr.length === 0) return [];
+    if (arr[i] === undefined) return sumArr;
+    // Recursive case
+    if (i === 1) sumArr = [arr[0]];
+
+    sumArr.push(sumArr[i - 1] + arr[i]);
+    return addUpToRecursion(arr, sumArr, i + 1);
+};
 
 console.log(addUpToRecursion([])); // []
-console.log(addUpToRecursion([10,20])); // [10,30]
+console.log(addUpToRecursion([10, 20])); // [10,30]
 console.log(addUpToRecursion([1, 2, 3, 0])); // [1, 3, 6, 6]
 
 /* 
@@ -78,3 +78,24 @@ Pass 1
 
 Pass 2, i = 2
 */
+
+
+
+function addUpTo(arr) {
+    let sum = 0;
+    return arr.reduce((acc, curr) => {
+        acc.push(sum += curr);
+        return acc;
+    }, []);
+}
+
+
+function addUpTo(arr) {
+    return arr.reduce((acc, curr) => (acc.push((acc.length ? acc[acc.length - 1] : 0) + curr), acc), []);
+}
+
+const addUpTo = arr => arr.reduce((acc, curr) => (acc.push((acc.length ? acc[acc.length - 1] : 0) + curr), acc), []);
+
+console.log(addUpTo([])); // []
+console.log(addUpTo([10, 20])); // [10,30]
+console.log(addUpTo([1, 2, 3, 0])); // [1, 3, 6, 6]
