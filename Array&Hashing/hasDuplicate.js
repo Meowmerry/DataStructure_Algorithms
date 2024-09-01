d9ca0a0ac1c332db0903443232bcc47f33cc678f
EdnaK18 <reginakwanrx@gmail.com>
2024-09-01 08:55:27 -0400

    solve twoSum and hasDuplicate

--- Array&Hashing/hasDuplicate.js
+++ Array&Hashing/hasDuplicate.js
@@ -6,12 +6,89 @@ Given an integer array nums, return true if any value appears more than once in 
 Example 1:
 
 Input: nums = [1, 2, 3, 3]
-
 Output: true
+
 Example 2:
-
 Input: nums = [1, 2, 3, 4]
-
 Output: false
 
+
+Input: nums = [-1, 2, 3, 3, -1]
+Output: true
+
+Explore:
+numbers can be pos, neg, or zero
+input is an array of numbers,
+output is a boolean
+
+edge cases:
+empty array should return...false
+
+
+Brainstorm:
+
+Time Complexity: O(N), where N is the length of the input array. This is because you only iterate through the array once.
+Space Complexity: O(N) in the worst case, where N is the length of the input array. This is because, in the worst case, 
+all elements are unique, so all elements are stored in the set.
+
+Plan: Using Set or Map to look up
+Iterate throuhg the array
+    if find dupplicate return true
+else return false
+
+Input: nums = [1, 2, 3, 3]
+Set = {1,2,3}
+Map
+{
+    1: 0
+    2: 0
+    3: 1
+}
+
+create new map
+iterate through the array
+  - check if the element exists in the map
+  - if yes - the element is a duplicate. return true
+
+  - if no, this is the first time we are seeing the element, so create the key
+
+  -if we reach the end of the array, there are no duplicates. return false
+
+
+
+
 */
+function hasDuplicate(nums) {
+  const dupMap = new Set();
+  for (i = 0; i < nums.length; i++) {
+    //check if the element at i exists in our map
+    const currNum = nums[i];
+    if (dupMap.has(currNum)) return true;
+    // else dupMap.add(currNum)
+    dupMap.add(currNum);
+  }
+
+  return false;
+}
+console.log(hasDuplicate([1, 2, 3, 3])); // True
+console.log(hasDuplicate([])); // False
+console.log(hasDuplicate([2])); // False
+console.log(hasDuplicate([-1, 2, 3, 3, -1])); // True
+console.log(hasDuplicate([-1, 2, 1, 3, -3])); // False
+
+
+// Using Map to look up
+function hasDuplicate1(nums) {
+  const duplicate = new Map();
+  for (const num of nums) {
+    duplicate.set(num, (duplicate.get(num) || 0) + 1);
+    if (duplicate.get(num) > 1) return true;
+  }
+
+  return false;
+}
+console.log(hasDuplicate1([1, 2, 3, 3])); // True
+console.log(hasDuplicate1([])); // False
+console.log(hasDuplicate1([2])); // False
+console.log(hasDuplicate1([-1, 2, 3, 3, -1])); // True
+console.log(hasDuplicate1([-1, 2, 1, 3, -3])); // False
--- Array&Hashing/twoSum.js
+++ Array&Hashing/twoSum.js
@@ -1,7 +1,8 @@
 /*
 Two Integer Sum
 https://neetcode.io/problems/two-integer-sum
-Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+Given an array of integers nums and an integer target, 
+return the indices i and j such that nums[i] + nums[j] == target and i != j.
 
 You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 
@@ -11,34 +12,96 @@ Example 1:
 
 Input: 
 nums = [3,4,5,6], target = 7
-
 Output: [0,1]
 Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
 
 Example 2:
-
 Input: nums = [4,5,6], target = 10
-
 Output: [0,2]
+
 Example 3:
-
 Input: nums = [5,5], target = 10
-
 Output: [0,1]
-Constraints:
 
+Constraints:
 2 <= nums.length <= 1000
 -10,000,000 <= nums[i] <= 10,000,000
 -10,000,000 <= target <= 10,000,000
 
+Explore:
+Input: array of nums >= 2 , target: Number
+Output: index of two sum // with the smaller index first
+target: is can be zero, or nagative or positive
+
+Brainstrom:
+    Brute force: 
+        2 loops
+     result = []       
+ [3,4,5,6], target = 7
+  
+
+  i + j = target
+  if(i+ j === tareget)
+       resutl[i, j]
+ 
+  return result
+ first iterate 
+    second inter
+
+
+
+    -----------
+
+    solution using Map
+
+    key:value
+    key = element, value = index
+
+    1 loop only. 
+    create a new Map
+    iterate through the array
+        check target - current element to find the difference
+        check our map if the difference exists, return [i, map[difference]] (but need to sort this output)
+{
+ element: Index 
+}
+        
+Plan: 
+
+
+Implement:
+
 
 */
 
-class Solution {
-    /**
-     * @param {number[]} nums
-     * @param {number} target
-     * @return {number[]}
-     */
-    twoSum(nums, target) { }
-}
+// class Solution {
+//     /**
+//      * @param {number[]} nums
+//      * @param {number} target
+//      * @return {number[]}
+//      */
+//     twoSum(nums, target) { }
+// }
+
+function twoSum(nums, target) {
+    const numsMap = new Map();
+    // const output = [];
+    for (i = 0; i < nums.length; i++){
+        let diff = target - nums[i];
+        // console.log({numsMap})
+        //check if we've seen the difference already
+            if (numsMap.has(diff)) {
+                 console.log('get:', numsMap.get(4));
+                 console.log('has:', numsMap.has(3));
+                return [numsMap.get(diff), i];
+            }
+            // output.push(Math.min(i,numsMap.get(diff)))
+            numsMap.set(nums[i], i);
+    }
+   
+    return [-1,-1]
+ }
+
+ console.log(twoSum([3,4,5,6],7)) // [0,1]
+//  console.log(twoSum([4,5,6],10)) // [0,2]
+//  console.log(twoSum([5,5],10)) // [0,1]
