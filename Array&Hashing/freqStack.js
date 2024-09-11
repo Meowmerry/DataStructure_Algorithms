560b2a93d83d2d2b77edae221b0a20b53e8552ae
MeowMerry <meow3525@gmail.com>
2024-09-11 11:25:49 -0500

    freqStack

---
+++ Array&Hashing/freqStack.js
@@ -0,0 +1,28 @@
+/*
+Frequency Stack
+
+Implement a stack that returns the most frequent element when the pop() method is called instead of the last element added. In the event of a tie, pop the last element added into the stack.
+ 
+
+EXAMPLE(S)
+FreqStack freqStack = new FreqStack();
+freqStack.push(5); // The stack is [5]
+freqStack.push(7); // The stack is [5,7]
+freqStack.push(5); // The stack is [5,7,5]
+freqStack.push(7); // The stack is [5,7,5,7]
+freqStack.push(4); // The stack is [5,7,5,7,4]
+freqStack.push(5); // The stack is [5,7,5,7,4,5]
+freqStack.pop();   // return 5, as 5 is the most frequent. 
+The stack becomes [5,7,5,7,4].
+freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+
+[5,7,5,7,4,5,4]
+pop --> 
+[5,7,5,7,4,4]
+
+FUNCTION SIGNATURE
+class FreqStack:
+
+*/
